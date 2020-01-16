11827b51bfc5ceac71f78b8b17f484e618d363f3
1903084 <1903084@uad.ac.uk>
2020-01-16 12:09:00 +0000

    Defined new objects

--- Week1/CMP105App/Level.h
+++ Week1/CMP105App/Level.h
@@ -18,7 +18,17 @@ private:
 	void beginDraw();
 	void endDraw();
 	sf::RenderWindow* window;
+	sf::Vector2u m_screenSize;
 
 	sf::RectangleShape rect;
 	sf::CircleShape circle;
+	sf::RectangleShape rectbotright;
+
+	sf::RectangleShape rectouter;
+	sf::RectangleShape rectmiddle;
+	sf::RectangleShape rectinner;
+
+	sf::Font font;
+	sf::Text text;
+	sf::Vector2f textscale;
 };
